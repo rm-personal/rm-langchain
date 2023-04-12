05874d9c0003aeeca6cf5d8ed91e9eee3ba5ba84
raghu <raghumish@gmail.com>
2023-04-12 14:00:59 +0530

    updated KEY VARIABLE

--- langchain_first_exp.py
+++ langchain_first_exp.py
@@ -22,7 +22,7 @@
 # In[7]:
 
 import os
-import open  
+import openai
 import streamlit as st # import the Streamlit library
 from langchain.chains import LLMChain, SimpleSequentialChain # import LangChain libraries
 from langchain.llms import OpenAI # import OpenAI model
